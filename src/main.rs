3a8d161379cce69b0d7274bbcff72cc1daebcb63
Andre0n <andreon098@gmail.com>
2021-12-27 17:40:38 -0300

    :sparkles: Generate a random number and prints it.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::io;
-fn main(){
+use rand::Rng;
+
+fn main() {
+    let stdin = io::stdin();
+    let mut rng = rand::thread_rng();
+
     println!("Guessing Game - Try to find the secret number.");
+    let secret_number:u32 = rng.gen_range(1..=100);
+
     println!("Please input your guess: ");
     let mut user_input = String::new();
-    let stdin = io::stdin();
     stdin.read_line(&mut user_input).expect("Erro while reading `input`");
     println!("Good job, you typed {}", user_input);
+
+    println!("The secret number is {}", secret_number);
 }
