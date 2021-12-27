ab8f91080c9b5a0582ab25336ce0d876219d14b1
Andre0n <andreon098@gmail.com>
2021-12-27 16:55:24 -0300

    :sparkles: Get  the user input and prints it.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::io;
 fn main(){
-    println!("Hello, world!");
+    println!("Guessing Game - Try to find the secret number.");
+    println!("Please input your guess: ");
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut user_input).expect("Erro while reading `input`");
+    println!("Good job, you typed {}", user_input);
 }
