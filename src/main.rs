27584535d83210897a6006cf9854f2b86720445e
Andre0n <andreon098@gmail.com>
2021-12-27 18:00:38 -0300

    ✨ Add a game loop.

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,24 @@ fn main() {
     println!("Guessing Game - Try to find the secret number.");
     let secret_number:u32 = rng.gen_range(1..=100);
 
-    println!("Please input your guess: ");
-    let mut user_input = String::new();
-    stdin.read_line(&mut user_input).expect("Erro while reading `input`");
-    let user_input:u32 = match user_input.trim().parse::<u32>() {
-        Ok(result) => result,
-        Err(..) => 0, // Temporary
-    };
-
-    match user_input.cmp(&secret_number) {
-        Ordering::Less => println!("Too small! Try again!"),
-        Ordering::Greater => println!("Too big! Try again!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Please input your guess: ");
+        let mut user_input = String::new();
+        stdin.read_line(&mut user_input).expect("Erro while reading `input`");
+        let user_input:u32 = match user_input.trim().parse::<u32>() {
+            Ok(result) => result,
+            Err(..) => {
+                println!("Nice try, but enter a valid number.");
+                continue;
+            },
+        };
+        match user_input.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! Try again!"),
+            Ordering::Greater => println!("Too big! Try again!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
